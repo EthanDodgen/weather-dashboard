8a039d667d4767b4e2d10ad64d5f02d578ec1931
Fival <thefivalco@gmail.com>
2020-12-29 08:19:36 -0600

    User City Input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,22 @@
+var userFormEl = document.querySelector("#user-form")
+var nameInputEl = document.querySelector("#username")
 
+//event handler
+var formSubmitHandler = function(event) {
+    event.preventDefault()
+
+    var username = nameInputEl.value.trim()
+
+    if (username) {
+        getUserCity(username)
+    } else {
+        alert("Please enter a city!")
+    }
+}
+
+//takes user input and fetches correct city in the API
 var getUserCity = function(user) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q=Nashville&appid=008d095a9cd21e032380d9cc37c980a7"
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+username.value+"&appid=008d095a9cd21e032380d9cc37c980a7"
   
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
@@ -9,6 +25,9 @@ var getUserCity = function(user) {
     })
   }
   getUserCity()
+
+//event listener
+  userFormEl.addEventListener("submit", formSubmitHandler);
  
 
 //get the users city from input and info from API
