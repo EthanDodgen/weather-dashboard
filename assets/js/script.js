fa7ac1f63add0ffc26732f9ade98c01d48f8c888
Fival <thefivalco@gmail.com>
2021-01-04 12:50:37 -0600

    update APi onecall

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,104 @@
 var userFormEl = document.querySelector("#user-form")
 var nameInputEl = document.querySelector("#username")
+var currentTime = moment().format("YYYY-MM-D hh:mm:ss")
+console.log(currentTime)
+var currentWeather = []
+console.log(currentWeather)
+
+
 
 //event handler
 var formSubmitHandler = function(event) {
-    event.preventDefault()
+    event.preventDefault()//cancels default action
 
     var cityName = nameInputEl.value.trim()
-        console.log(cityName)
+    console.log(cityName)
 
     if (cityName) {
-        getUserCity(cityName)
+        getUserInfo(cityName)
     } else {
         alert("Please enter a city!")
     }
 }
 
-//takes user input and fetches correct city in the API
-var getUserCity = function(city) {
-    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+city+"&appid=008d095a9cd21e032380d9cc37c980a7"
-  
+//takes city name from handler, fetches API data, sends to displayCity variable 
+var getUserInfo = function(city) {//where does city come from and why does it work with any word?
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid=008d095a9cd21e032380d9cc37c980a7"
+    
     fetch(apiUrl).then(function(response) {
       response.json().then(function(data) {
-        console.log(data)
-      })
+      displayCity(data)
     })
-  }
+  })
+}
+
+//receives/saves API data??
+var displayCity = function(searchedCity) {
+    console.log(searchedCity)//where is searchedCity coming from?
+
+    var lat = searchedCity.coord.lat
+    console.log(lat)
+    var lon = searchedCity.coord.lon
+    console.log(lon)
+
+    var apiUrl2 = "https://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude={part}&appid=6149d2b1c6ef07dcbfaf0143ed91da08"
+
+    fetch(apiUrl2).then(function(response) {
+      response.json().then(function(data) {
+      console.log(data)
+    })
+  })
+}
+
+
   
+
+
+
+
+    
+
+    //displays current weather
+    //currentWeather.push(searchedCity.name)
+    //currentWeather.push(searchedCity.main.temp)
+    //currentWeather.push(searchedCity.main.humidity)
+    //currentWeather.push(searchedCity.wind.speed)
+
+
+
+  //loop over array
+  //for(var i = 0; i < searchedCity.list; i++) {
+    
+        //var searchTime = searchedCity.list[i].dt_txt
+          //console.log(searchTime)
+
+    //if (currentTime < searchTime) {
+        //console.log("Works")
+
+
+  //}
+
+
+
+
+  //var getOneCall = function() {
+ 
+
+
+
+
+
+
+
 //event listener
   userFormEl.addEventListener("submit", formSubmitHandler)
+
+
+
+ //next steps
+  //*obtain data needed from API and display with city
+
+  //create blank cards to be appended 
  
 //display current weather
 
@@ -35,9 +106,7 @@ var getUserCity = function(city) {
 
 //save to local storage
 
+//display to search list
+
 
 //create search history
-
-
-
-
